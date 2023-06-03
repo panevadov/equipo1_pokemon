6f836c2b17969ce92af064c8758be22248a0bce9
andresgamo <andresgarciamolina3@gmail.com>
2023-06-03 12:29:26 -0500

    solve problem showing card values.

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -6,30 +6,29 @@ const section = document.querySelector('section');
 async function fetchData(url){
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-
     pokemonData.push({
         name: data.name,
         hp: data.stats[0].base_stat,
         img: data.sprites.other['official-artwork'].front_default,
+        level: data.base_experience,
     });
 
     const card = document.createElement('div');
     card.classList.add('main-container');
     card.innerHTML = ` <div class="up-container inline">
-                            <p>${pokemonData.name}</p>
-                            <a href="">${pokemonData.hp}</a>
+                            <p>${pokemonData[0].name}</p>
+                            <a href="">${pokemonData[0].hp}</a>
                         </div>
                         <div>
-                            <img  src="${pokemonData.img}" alt="charmander">
+                            <img  src="${pokemonData[0].img}" alt="charmander">
                         </div>
                         <div  class="down-container inline">
-                            <p>Power level</p>
+                            <p>${pokemonData[0].level}</p>
                             <input type="button" value="Buy">
                         </div>`;
 
     section.appendChild(card);
-    }
-    
+}
+
 
 fetchData(url);
